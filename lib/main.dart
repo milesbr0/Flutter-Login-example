5668cba4b699e3f92b9ef4b04a6a74effd67323f
Richard Kiss <32391824+milesbr0@users.noreply.github.com>
2019-05-16 11:52:48 +0200

    Gmail login

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -17,8 +18,24 @@ class MyMainPage extends StatefulWidget {
 class _MyMainPageState extends State<MyMainPage> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   bool isLoggedIn = false;
-  FirebaseUser myUser;
+  bool isLoading = false;
+  FirebaseUser currentUser;
 
+  //Google Login
+  Future<FirebaseUser> _loginWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuth.accessToken,
+        accessToken: googleAuth.accessToken,
+    );
+    FirebaseUser firebaseUser = await _auth.signInWithCredential(credential);
+    return firebaseUser;
+  }
+
+  //Facebook Login
   Future<FirebaseUser> _loginWithFacebook() async {
     var facebookLogin = new FacebookLogin();
     var result = await facebookLogin.logInWithReadPermissions(['email']);
@@ -34,10 +51,20 @@ class _MyMainPageState extends State<MyMainPage> {
     return null;
   }
 
-  void _logIn(){
+  void _logInGM(){
+    _loginWithGoogle().then((response){
+      if(response != null){
+        currentUser = response;
+        isLoggedIn = true;
+        setState(() {});
+      }
+    });
+  }
+
+  void _logInFB(){
     _loginWithFacebook().then((response){
       if(response != null){
-        myUser = response;
+        currentUser = response;
         isLoggedIn = true;
         setState(() {});
       }
@@ -60,22 +87,46 @@ class _MyMainPageState extends State<MyMainPage> {
         IconButton(
           icon: Icon(Icons.power_settings_new),
           onPressed: _logOut,
-        )
-      ],),
-      body: Center(
+        ),
+      ],
+      ),
+
+
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Expanded(
+            child: FacebookSignInButton(
+              onPressed: _logInFB,
+            )
+          ),
+          Expanded(
+              child: GoogleSignInButton(
+                onPressed: _logInGM,
+              )
+          ),
+        ],
+      )
+
+      /*body: Container(
+        child: Center(
         child: isLoggedIn
             ? Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-              Text('Name: ' + myUser.displayName),
-            Image.network(myUser.photoUrl),
+              Text('Name: ' + currentUser.displayName),
+            Image.network(currentUser.photoUrl),
         ],
 
         )
             : FacebookSignInButton(
-                onPressed: _logIn,
+                onPressed: _logInFB,
+        )
+
         ),
-      ),
+
+      )*/
     );
   }
 }
